fddc2978859b780f36b88d008c394bbc26426d4f
Bingyu Xia <71547343+BryceT233@users.noreply.github.com>
2026-02-24 13:30:43 +0800

    fix

--- Mathlib/RingTheory/MvPowerSeries/Trunc.lean
+++ Mathlib/RingTheory/MvPowerSeries/Trunc.lean
@@ -55,7 +55,7 @@ public import Mathlib.RingTheory.AdicCompletion.Algebra
   multivariate power series to the quotient ring of multivariate plynomials at
   its `n`-th power of the ideal spanned by all variables.
 
-* `MvPowerSeries.toAdicCompletionAlgEquiv` : the canonical isomorphism from
+* `MvPowerSeries.truncTotalToAdicCompletionAlgEquiv` : the canonical isomorphism from
   multivariate power series to the adic completion of multivariate polynomials at
   the ideal spanned by all variables when the index is finite.
 
@@ -377,7 +377,7 @@ def truncTotalAlgHom (σ R : Type*) [Finite σ] [CommRing R] (n : ℕ) :
 
 /-- The canonical map from multivariate power series to the adic completion of
 multivariate polynomials at the ideal spanned by all variables when the index is finite. -/
-def toAdicCompletion (σ R : Type*) [Finite σ] [CommRing R] :
+def truncTotalToAdicCompletion (σ R : Type*) [Finite σ] [CommRing R] :
     MvPowerSeries σ R →ₐ[MvPolynomial σ R]
       AdicCompletion (MvPolynomial.idealOfVars σ R) (MvPolynomial σ R) :=
   AdicCompletion.liftAlgHom (MvPolynomial.idealOfVars σ R) (truncTotalAlgHom σ R)
@@ -385,46 +385,46 @@ def toAdicCompletion (σ R : Type*) [Finite σ] [CommRing R] :
       simpa [Ideal.Quotient.mk_eq_mk_iff_sub_mem] using
         truncTotal_sub_truncTotal_mem_pow_idealOfVars h (le_refl _) _)
 
-lemma toAdicCompletion_apply_eq_mk_truncTotal {n : ℕ} {p : MvPowerSeries σ R} :
-    (toAdicCompletion σ R p).val n = truncTotal R n p := by rfl
+lemma truncTotalToAdicCompletion_apply_eq_mk_truncTotal {n : ℕ} {p : MvPowerSeries σ R} :
+    (truncTotalToAdicCompletion σ R p).val n = truncTotal R n p := by rfl
 
-theorem coeff_toAdicCompletion_val_apply_out {x : σ →₀ ℕ} {p : MvPowerSeries σ R} {n : ℕ}
-    (hx : degree x < n) : (Quotient.out (((toAdicCompletion σ R) p).val n)).coeff x =
+theorem coeff_truncTotalToAdicCompletion_val_apply_out {x : σ →₀ ℕ} {p : MvPowerSeries σ R} {n : ℕ}
+    (hx : degree x < n) : (Quotient.out (((truncTotalToAdicCompletion σ R) p).val n)).coeff x =
       (coeff x) p := by
   rw [← coeff_truncTotal _ hx, ← sub_eq_zero, ← MvPolynomial.coeff_sub]
   apply (MvPolynomial.mem_pow_idealOfVars_iff' n _).mp
-  · rw [toAdicCompletion_apply_eq_mk_truncTotal, smul_eq_mul]
+  · rw [truncTotalToAdicCompletion_apply_eq_mk_truncTotal, smul_eq_mul]
     nth_rw 1 [← Ideal.mul_top (MvPolynomial.idealOfVars σ R ^ n), ← Ideal.Quotient.eq,
       Ideal.Quotient.mk_out]
   exact hx
 
-theorem toAdicCompletion_coe (p : MvPolynomial σ R) :
-    toAdicCompletion σ R p = .of (MvPolynomial.idealOfVars σ R) (MvPolynomial σ R) p := by
+theorem truncTotalToAdicCompletion_coe (p : MvPolynomial σ R) :
+    truncTotalToAdicCompletion σ R p = .of (MvPolynomial.idealOfVars σ R) (MvPolynomial σ R) p := by
   symm; ext n
   suffices p - (truncTotal R n) p ∈ MvPolynomial.idealOfVars σ R ^ n by
-    simpa [toAdicCompletion, AdicCompletion.liftAlgHom, AdicCompletion.liftRingHom,
+    simpa [truncTotalToAdicCompletion, AdicCompletion.liftAlgHom, AdicCompletion.liftRingHom,
       Ideal.Quotient.mk_eq_mk_iff_sub_mem]
   exact (MvPolynomial.mem_pow_idealOfVars_iff' ..).mpr fun x hx ↦ by simp [coeff_truncTotal _ hx]
 
-/-- An inverse function of `toAdicCompletion`. -/
-def toAdicCompletionInv (σ R : Type*) [CommRing R]
+/-- An inverse function of `truncTotalToAdicCompletion`. -/
+def truncTotalToAdicCompletionInv (σ R : Type*) [CommRing R]
     (f : AdicCompletion (MvPolynomial.idealOfVars σ R) (MvPolynomial σ R)) :
       MvPowerSeries σ R := fun x ↦ (f.val (degree x + 1)).out.coeff x
 
 omit [Finite σ] in
-lemma coeff_toAdicCompletionInv {x : σ →₀ ℕ}
+lemma coeff_truncTotalToAdicCompletionInv {x : σ →₀ ℕ}
     {f : AdicCompletion (MvPolynomial.idealOfVars σ R) (MvPolynomial σ R)} :
-      coeff x (toAdicCompletionInv σ R f) = (f.val (degree x + 1)).out.coeff x := by rfl
+      coeff x (truncTotalToAdicCompletionInv σ R f) = (f.val (degree x + 1)).out.coeff x := by rfl
 
-theorem mk_truncTotal_toAdicCompletionInv {n : ℕ}
+theorem mk_truncTotal_truncTotalToAdicCompletionInv {n : ℕ}
     {f : AdicCompletion (MvPolynomial.idealOfVars σ R) (MvPolynomial σ R)} :
       Ideal.Quotient.mk (MvPolynomial.idealOfVars σ R ^ n • ⊤)
-    ((truncTotal R n) (toAdicCompletionInv σ R f)) = f.val n := by
+    ((truncTotal R n) (truncTotalToAdicCompletionInv σ R f)) = f.val n := by
   rw [← Ideal.Quotient.mk_out (f.val n), Ideal.Quotient.mk_eq_mk_iff_sub_mem]
   simp only [smul_eq_mul, Ideal.mul_top, MvPolynomial.mem_pow_idealOfVars_iff',
     MvPolynomial.coeff_sub]
   intro x h
-  rw [coeff_truncTotal _ h, coeff_toAdicCompletionInv, ← MvPolynomial.coeff_sub]
+  rw [coeff_truncTotal _ h, coeff_truncTotalToAdicCompletionInv, ← MvPolynomial.coeff_sub]
   apply (MvPolynomial.mem_pow_idealOfVars_iff' (degree x + 1) _).mp
   · nth_rw 1 [← Ideal.mul_top (MvPolynomial.idealOfVars σ R ^ (degree x + 1)),
       ← smul_eq_mul, ← Ideal.Quotient.eq]
@@ -435,23 +435,23 @@ theorem mk_truncTotal_toAdicCompletionInv {n : ℕ}
 
 /-- The isomorphism from multivariate power series to the adic completion of
 multivariate polynomials at the ideal spanned by all variables when the index is finite. -/
-def toAdicCompletionAlgEquiv (σ R : Type*) [Finite σ] [CommRing R] :
+def truncTotalToAdicCompletionAlgEquiv (σ R : Type*) [Finite σ] [CommRing R] :
     MvPowerSeries σ R ≃ₐ[MvPolynomial σ R]
       AdicCompletion (MvPolynomial.idealOfVars σ R) (MvPolynomial σ R) where
-  __ := toAdicCompletion σ R
-  invFun := toAdicCompletionInv σ R
+  __ := truncTotalToAdicCompletion σ R
+  invFun := truncTotalToAdicCompletionInv σ R
   left_inv _ := by
-    ext; simp [coeff_toAdicCompletionInv, coeff_toAdicCompletion_val_apply_out]
-  right_inv _ := by ext; simpa using mk_truncTotal_toAdicCompletionInv
+    ext; simp [coeff_truncTotalToAdicCompletionInv, coeff_truncTotalToAdicCompletion_val_apply_out]
+  right_inv _ := by ext; simpa using mk_truncTotal_truncTotalToAdicCompletionInv
 
 @[simp]
-lemma toAdicCompletionAlgEquiv_apply (p : MvPowerSeries σ R) :
-    toAdicCompletionAlgEquiv σ R p = toAdicCompletion σ R p := by rfl
+lemma truncTotalToAdicCompletionAlgEquiv_apply (p : MvPowerSeries σ R) :
+    truncTotalToAdicCompletionAlgEquiv σ R p = truncTotalToAdicCompletion σ R p := by rfl
 
 @[simp]
-lemma toAdicCompletionAlgEquiv_symm_apply
+lemma truncTotalToAdicCompletionAlgEquiv_symm_apply
     (x : AdicCompletion (MvPolynomial.idealOfVars σ R) (MvPolynomial σ R)) :
-      (toAdicCompletionAlgEquiv σ R).symm x = toAdicCompletionInv σ R x := by rfl
+      (truncTotalToAdicCompletionAlgEquiv σ R).symm x = truncTotalToAdicCompletionInv σ R x := by rfl
 
 end TruncTotal
 
