0a3163f500e5d6d24c4fa66e4b352e4407e30823
Bingyu Xia <71547343+BryceT233@users.noreply.github.com>
2026-02-25 10:25:05 +0800

    fix

--- Mathlib/Data/Finsupp/Weight.lean
+++ Mathlib/Data/Finsupp/Weight.lean
@@ -269,7 +269,7 @@ theorem degree_mapDomain_eq {M τ : Type*} (f : σ → τ) [AddCommMonoid M] [Su
   trans (x.mapDomain f).sum (fun _ ↦ id)
   · simp [degree, sum]
   simpa [sum, mapDomain_support_of_subsingletonAddUnits, degree] using Finset.sum_image' _
-    (fun _ _ ↦ mapDomain_apply_eq_sum)
+    (fun _ _ ↦ mapDomain_apply_eq_sum ..)
 
 theorem degree_comapDomain_le {M τ : Type*} {f : σ → τ} [AddCommMonoid M] [PartialOrder M]
     [CanonicallyOrderedAdd M] {x : τ →₀ M} (hf : Set.InjOn f (f ⁻¹' x.support)) :
